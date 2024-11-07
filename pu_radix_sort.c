3cbf22bace2d1b8ee6ae3f883d153c2b9f85eee8
Alejandro Hernández Hernández <alejhern@student.42barcelona.com>
2024-11-07 22:10:04 +0100

    little error print

--- pu_radix_sort.c
+++ pu_radix_sort.c
@@ -6,7 +6,7 @@
 /*   By: amhernandez <alejhern@student.42.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 21:20:03 by amhernandez       #+#    #+#             */
-/*   Updated: 2024/10/30 11:07:48 by amhernandez      ###   ########.fr       */
+/*   Updated: 2024/11/07 22:09:27 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,13 +112,13 @@ void radix_sort(char ***stack_a, char ***stack_b)
             if ((num >> bit) & 1)
                 ra(stack_a, 1);
             else
-                pa(stack_b, stack_a, 1);
+                pb(stack_b, stack_a, 1);
             move_count++; // Incrementar el contador de movimientos
             i++;
         }
         while (*stack_b && (*stack_b)[0])
         {
-            pb(stack_a, stack_b,  1);
+            pa(stack_a, stack_b,  1);
             move_count++; // Incrementar el contador de movimientos
         }
         bit++;
